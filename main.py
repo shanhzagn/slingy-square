a2cb8e6577279dc71cd7e58778f9fb4612d535e8
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-08-07 12:37:56 +1000

    created clock and display

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import funcs
+import pygame
+from sys import exit
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Slingy Square")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+pygame.display.update()
+clock.tick(60)
